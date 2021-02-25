10eada1424dbd5c45e3649865ecb354ebb4ffa6b
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2021-02-25 09:24:11 -0300

    ola mundo

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,11 @@
+class OlaMundo{
+    public static void main(String[] args) {
+        //comentario de uma linha
+        /*comentario
+        de
+        varias
+        linhas
+        */
+        System.out.println("Ola Mundo!");
+    }
+}
