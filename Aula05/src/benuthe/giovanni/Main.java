336768bf2102b949deada4c9a93af922f5f9b9ef
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2021-03-15 09:33:41 -0300

    Aula 05
    Alteração da Aula 03
    Início da construção do MauaBank
    Lab. de segunda 15/03/21

---
+++ Aula05/src/benuthe/giovanni/Main.java
@@ -0,0 +1,11 @@
+package benuthe.giovanni;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Sistema sistema = new Sistema();
+        sistema.executar();
+
+    }
+}
