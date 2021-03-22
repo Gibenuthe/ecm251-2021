0469b844fd393036ae41459baa271d6c194a4bc3
Gibenuthe <79452579+Gibenuthe@users.noreply.github.com>
2021-03-22 08:58:01 -0300

    Criação da Atividade
    - criando o projeto e classes obrigatorias

---
+++ Atividade01/src/benuthe/giovanni/Conta.java
@@ -0,0 +1,8 @@
+/*
+        NOME: Giovanni Brandini Blanco Benuthe
+        RA:   19.00043-0
+ */
+package benuthe.giovanni;
+
+public class Conta {
+}
---
+++ Atividade01/src/benuthe/giovanni/Main.java
@@ -0,0 +1,12 @@
+/*
+        NOME: Giovanni Brandini Blanco Benuthe
+        RA:   19.00043-0
+ */
+package benuthe.giovanni;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
